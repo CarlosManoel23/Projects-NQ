0cba07c28f5f6ba2f0b91c6837a907797a9f31ec
CarlosManoel23 <carlosbatista0145@gmail.com>
2025-12-26 11:22:56 -0300

    adicionando um loader

---
+++ scripts/utilities/loader.js
@@ -0,0 +1,6 @@
+window.addEventListener('load', () => {
+    const loader = document.getElementById('loader');
+    
+    // Adiciona a classe que faz o loader sumir suavemente
+    loader.classList.add('loader-hidden');
+});
